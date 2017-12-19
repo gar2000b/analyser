4c21840ca9969942c9f51aabab56e356a300ccc0
Gary Black <gar2000b@yahoo.com>
2017-12-19 13:03:47 +0000

    More enhancements

--- src/main/java/com/onlineinteract/Analyser.java
+++ src/main/java/com/onlineinteract/Analyser.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import com.onlineinteract.enumeration.Status;
 
@@ -12,9 +14,12 @@ public class Analyser {
 	private static final int SAMPLE_LENGTH = 3045859;
 	private String[][] samples;
 	private double previousClose = 0.0;
-	private double origFallingValue = 0.0;
+	private double origValue = 0.0;
 	private double lowestPercentageDip = 0.0;
 	Status status = Status.SAME;
+	private String origSummary;
+	private double lowestClose;
+	private String lowestDate;
 
 	public Analyser() {
 		BufferedReader br = null;
@@ -39,55 +44,72 @@ public class Analyser {
 			}
 			System.out.println("i is: " + counter);
 			System.out.println("File read complete...");
-			System.out.println(samples[0][0] + ", " + samples[0][1] + ", " + samples[0][2] + ", "
-					+ samples[0][3] + ", " + samples[0][4] + ", " + samples[0][5] + ", "
-					+ samples[0][6] + ", " + samples[0][7]);
-			System.out.println(samples[1][0] + ", " + samples[1][1] + ", " + samples[1][2] + ", "
-					+ samples[1][3] + ", " + samples[1][4] + ", " + samples[1][5] + ", "
-					+ samples[1][6] + ", " + samples[1][7]);
+			System.out.println(samples[0][0] + ", " + samples[0][1] + ", " + samples[0][2] + ", " + samples[0][3] + ", "
+					+ samples[0][4] + ", " + samples[0][5] + ", " + samples[0][6] + ", " + samples[0][7]);
+			System.out.println(samples[1][0] + ", " + samples[1][1] + ", " + samples[1][2] + ", " + samples[1][3] + ", "
+					+ samples[1][4] + ", " + samples[1][5] + ", " + samples[1][6] + ", " + samples[1][7]);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public void detect4PercentSpiral() {
-		for(int i = 1; i < samples.length - 1; i++) {
+		for (int i = 1; i < samples.length - 1; i++) {
 			double currentClose = Double.valueOf(samples[i][4]);
-			if(currentClose < previousClose) {
+			if (currentClose < previousClose) {
 				// System.out.println("Falling");
-				if(origFallingValue == 0.0) {
-					origFallingValue = currentClose;
+				if (origValue == 0.0) {
+					origValue = previousClose;
+					long timestamp = 1000 * Long.valueOf(samples[i][0]);
+					SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy @ HH:mm:ss");
+					String date = sdf.format(new Date(timestamp));
+					origSummary = "\nSpiral Date: " + date + " - " + samples[i][0] + " - CurrentClose: " + currentClose
+							+ " - OrigValue: " + origValue;
 				}
 				// work out percentage dip from orig
-				double diff = origFallingValue - currentClose;
-				//System.out.println("Diff is: " + diff);
-				double percentage = (100/origFallingValue) * diff;
-				//System.out.println("Percentage is: " + percentage);
-				//System.out.println("lowestPercentageDip is: " + lowestPercentageDip);
+				double diff = origValue - currentClose;
+				// System.out.println("Diff is: " + diff);
+				double percentage = (100 / origValue) * diff;
+				// System.out.println("Percentage is: " + percentage);
+				// System.out.println("lowestPercentageDip is: " +
+				// lowestPercentageDip);
 				// determine if lowest percentage dip during spiral
 				if (percentage > lowestPercentageDip) {
 					lowestPercentageDip = percentage;
+					lowestClose = currentClose;
+					long timestamp = 1000 * Long.valueOf(samples[i][0]);
+					SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy @ HH:mm:ss");
+					lowestDate = sdf.format(new Date(timestamp));
 				}
-				
+
 				status = Status.FALLING;
-			} else  if (currentClose == previousClose) {
+			} else if (currentClose == previousClose) {
 				// System.out.println("Same");
 				status = Status.SAME;
 			} else if (currentClose > previousClose) {
 				// System.out.println("Rising");
 				// end spiral is current = orig
-				if (currentClose >= origFallingValue && origFallingValue != 0.0 && lowestPercentageDip != 0.0) {
-					origFallingValue = 0.0;
-					System.out.println("Lowest percentage dip = " + lowestPercentageDip);
+				if (currentClose >= origValue && origValue != 0.0 && lowestPercentageDip != 0.0) {
+					if (lowestPercentageDip > 1) {
+						System.out.println(origSummary);
+						System.out.println(
+								"Lowest Date: " + lowestDate + " - Lowest percentage dip = " + lowestPercentageDip + " - Lowest Close: " + lowestClose);
+						long timestamp = 1000 * Long.valueOf(samples[i][0]);
+						SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy @ HH:mm:ss");
+						String date = sdf.format(new Date(timestamp));
+						System.out.println("Recovery Date: " + date + " - " + samples[i][0] + " - CurrentClose: "
+								+ currentClose + " - PreviousClose: " + previousClose + " - OrigValue: " + origValue);
+					}
+					origValue = 0.0;
 					lowestPercentageDip = 0.0;
 				}
 				status = Status.RISING;
 			}
 			previousClose = currentClose;
-//			System.out.println(currentClose);
+			// System.out.println(currentClose);
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Analyser analyser = new Analyser();
 		analyser.detect4PercentSpiral();
